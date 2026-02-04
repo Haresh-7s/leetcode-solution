42328952f47e4fdd51b2f25403207e15ec9fbc69
Haresh-7s <24149014@drngpit.ac.in>
2026-02-04 14:00:22 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,6 @@
+// Last updated: 2/4/2026, 2:00:23 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        return (num==6 || num==28|| num==496|| num==33550336||num==8128);
+    }
+}
