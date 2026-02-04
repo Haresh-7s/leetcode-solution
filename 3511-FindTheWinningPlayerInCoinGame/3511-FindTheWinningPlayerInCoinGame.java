db112d6b0efc3762a8778937e0b9c55e2b8c898b
Haresh-7s <24149014@drngpit.ac.in>
2026-02-04 13:59:49 +0530

    Create 3511-FindTheWinningPlayerInCoinGame

---
+++ 3511-FindTheWinningPlayerInCoinGame/3511-FindTheWinningPlayerInCoinGame.java
@@ -0,0 +1,14 @@
+// Last updated: 2/4/2026, 1:59:50 PM
+class Solution {
+    public String winningPlayer(int x, int y) {
+        int turn =1;
+      while (x>=1 && y>=4){
+            x-=1;
+            y-=4;
+            turn++;
+        }
+        if(turn%2==0)return "Alice";
+        else return "Bob";
+        
+    }
+}
